6a21fa9aec149b7da30dce5ebc14beb5f134d102
mk <manojklm@gmail.com>
2015-02-02 11:14:08 +0530

    fix for summary report

--- pytest_filter.py
+++ pytest_filter.py
@@ -38,8 +38,8 @@ def pytest_report_header(config, startdir):
         print('filter: %s' % config.inicfg['filter_file'])
     else:
         print('filter: "filter_file" key was not found in [pytest] section')
-    if not config.option.filter_exclude_all:
-        print('filter: all tests will be excluded before including any from filter file')
+    if config.option.filter_exclude_all:
+        print('filter-exclude-all: tests are excluded before processing items in filter file')
 
 
 @pytest.mark.trylast
